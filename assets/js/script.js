eea396ad958d45d31a87896ac67a76e8f504aed8
Emerson Jr <emerson_jdss@hotmail.com>
2017-05-26 16:42:00 -0300

    Organization of files

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+var claimCopy = ["Tipo um banco ," + '<br/>' + "só que mais seguro."];
+
+    document.addEventListener('DOMContentLoaded', function(){
+      Typed.new('.first-title', {
+        strings: claimCopy,
+        typeSpeed: 70,
+        backDelay: 4000,
+        loop: true,
+        contentType: 'html', 
+        loopCount: false,
+        backspace: function(curString, curStrPos){
+          setTimeout(function() {
+              if (self.arrayPos == 1){
+                self.stopNum = 20;
+              }
+              else{
+                self.stopNum = 0;
+              }
+            });
+        }
+      });
+    });
---
+++ assets/js/slider.js
@@ -0,0 +1,36 @@
+var controls = document.querySelectorAll('.controls');
+
+controls.forEach((control) => {
+	control.style.display = 'inline-block';
+})
+
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+
+function proximoSlide() {
+	nextSlide();
+}
+
+function nextSlide(){
+	goToSlide(currentSlide + 1)
+}
+
+function backSlide() {
+	goToSlide(currentSlide - 1)
+}
+
+function goToSlide(n){
+	slides[currentSlide].className = 'slide';
+	currentSlide = (n+slides.length)%slides.length;
+	slides[currentSlide].className = 'slide showing';
+}
+
+var next = document.getElementById('next');
+var previous = document.getElementById('previous');
+
+next.onclick = function(){
+    nextSlide();
+};
+previous.onclick = function(){
+    backSlide()
+};
